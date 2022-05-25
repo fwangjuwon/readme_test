ceecf667e062209ad242270b777bfdd1c287d979
fwangjuwon <fwangjuwon@naver.com>
2022-05-25 15:43:30 +0900

    readme test

---
+++ src/main/java/site/metacoding/readmetest/ReadmetestApplication.java
@@ -0,0 +1,13 @@
+package site.metacoding.readmetest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReadmetestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReadmetestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/site/metacoding/readmetest/ReadmetestApplicationTests.java
@@ -0,0 +1,13 @@
+package site.metacoding.readmetest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReadmetestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
